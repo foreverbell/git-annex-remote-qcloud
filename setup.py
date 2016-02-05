e4e0ed1078f4d7f1379f5ea12ae9c16e6f64de5d
foreverbell <dql.foreverbell@gmail.com>
2016-02-05 18:12:41 +0800

    pypi prepare

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(name='git-annex-remote-qcloud',
       license='MIT',
       keywords='git-annex qcloud',
       description='git-annex external special remote protocol for qcloud (Tencent Cloud).',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7'
+      ],
       packages=['git_annex_remote_qcloud'],
       install_requires=['requests'],
       entry_points={'console_scripts': ['git-annex-remote-qcloud = git_annex_remote_qcloud.main:main']})
